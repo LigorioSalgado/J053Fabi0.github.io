2d27674d579d208232741374c525c8244f970475
J053Fabi0 <J053Fabi0@users.noreply.github.com>
2020-07-12 14:42:37 -0600

    Estoy empezando a trabajar en la base de datos. Ya tiene la parte de transporte

---
+++ HuellaCarbono/src/formas.js
@@ -0,0 +1,199 @@
+const formas = [
+  {
+    categoria: "Transporte",
+    cards: [
+      {
+        titulo: "Evitar un vuelo largo",
+        resumen: "Los combustibles necesarios para un vuelo de avión son ",
+        resto:
+          "muy contaminantes. Siempre es más ecológico ir en coche, en camión o incluso en tren, aunque esto no siempre sea lo más económico, claro.",
+        min: 0.7,
+        max: 4.5,
+        med: 1.9,
+      },
+      {
+        titulo: "Evitar un vuelo corto",
+        resumen: "Al evitar un vuelo corto también puedes ayudar a reducir ",
+        resto:
+          "tu huella de carbono. Viajar en coche, camión o tren es mejor, aunque sea menos económico e incluso práctico.",
+        min: 0.2,
+        max: 1.5,
+        med: 0.6,
+      },
+      {
+        titulo: "Usar transporte público o alternativa",
+        resumen: "Si tienes la oportunidad de ir en vicicleta o en ",
+        resto:
+          "transporte público, puedes reducir drásticamente tus emisiones de carbono.",
+        min: 0.6,
+        max: 1,
+        med: 0.8,
+      },
+      {
+        titulo: "Compartir transporte privado",
+        resumen: "Si compartes tu coche o le dices a un conocido que te ",
+        resto:
+          "lleve en su coche a un lugar a donde él también va en vez de usar tu coche para ti solito. Y quién sabe, quizá incluso te ayude a no gastar tanto dinero.",
+        min: null,
+        max: null,
+        med: 0.3,
+      },
+      {
+        titulo: "Evitar un vuelo largo",
+        resumen: "Los combustibles necesarios para un vuelo de avión son ",
+        resto:
+          "muy contaminantes. Siempre es más ecológico ir en coche, en camión o incluso en tren, aunque esto no siempre sea lo más económico, claro.",
+        min: 0.7,
+        max: 4.5,
+        med: 1.9,
+      },
+      {
+        titulo: "Evitar un vuelo corto",
+        resumen: "Al evitar un vuelo corto también puedes ayudar a reducir ",
+        resto:
+          "tu huella de carbono. Viajar en coche, camión o tren es mejor, aunque sea menos económico e incluso práctico.",
+        min: 0.2,
+        max: 1.5,
+        med: 0.6,
+      },
+      {
+        titulo: "Usar transporte público o alternativa",
+        resumen: "Si tienes la oportunidad de ir en vicicleta o en transporte ",
+        resto:
+          "público, puedes reducir drásticamente tus emisiones de carbono. No siempre es necesario usar tu coche para ir a un lugar. <b>¡Pero cuidado!</b> No pidas un taxi o similar, porque ese tipo de servicio consume hasta un 47% de su combustible sin un pasajero ni rumbo.",
+        min: 0.6,
+        max: 1,
+        med: 0.8,
+      },
+      {
+        titulo: "Compartir transporte privado",
+        resumen: "Si compartes tu coche o le dices a un conocido que te ",
+        resto:
+          "lleve en su coche a un lugar a donde él también va en vez de usar tu coche para ti solito. Y quién sabe, quizá incluso te ayude a no gastar tanto dinero.",
+        min: null,
+        max: null,
+        med: 0.3,
+      },
+      {
+        titulo: "Conducir eficientemente",
+        resumen: "No, quizá no lo estés haciendo. Lee las siguientes ",
+        resto:
+          "recomendaciones para manejar eficientemente: <a href='https://www.conduccioneficiente.cl'>Conducción eficiente</a>. Ahí se muestran muchos tips para ahorrar combustible. Te ayudará también a ahorrar dinero.",
+        min: null,
+        max: null,
+        med: 0.3,
+      },
+      {
+        titulo: "Cambiarse a un híbrido",
+        resumen: "¡Cuidado! Esto tiene trampa: si la red eléctrica de ",
+        resto:
+          "tu localidad genera la energía con combustión fósil, termina siento mejor usar un coche de gasolina. Además, no debes cambiar de coche a menos que tengas por lo menos 76,000 KM, aproximadamente. Por esto mismo, resulta que hacer este cambio puede ser incluso contraproducente, dependiendo de dónde vivas.",
+        min: -0.2,
+        max: 3.1,
+        med: 0.7,
+      },
+      {
+        titulo: "Carro 100% eléctrico",
+        resumen: "¡Cuidado! Esto tiene trampa: si la red eléctrica de ",
+        resto:
+          "tu localidad genera la energía con combustión fósil, termina siento mejor usar un coche de gasolina. Además, no debes cambiar de coche a menos que tengas por lo menos 76,000 KM, aproximadamente. Por esto mismo, resulta que hacer este cambio puede ser incluso contraproducente, dependiendo de dónde vivas.",
+        min: -1.9,
+        max: 5.4,
+        med: 2,
+      },
+      {
+        titulo: "Vivir sin coche",
+        resumen: "No compres un coche. Ten a la mano la vicicleta y usa ",
+        resto: "transportes públicos o compartidos como alternativa.",
+        min: 0.6,
+        max: 3.6,
+        med: 0,
+      },
+      {
+        titulo: "Trabajar desde casa",
+        resumen: "Así no tines que usar un transporte.",
+        resto: "",
+        min: 0.1,
+        max: 1.4,
+        med: 0.4,
+      },
+    ],
+  },
+  {
+    categoria: "Dieta",
+    cards: [
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+      {
+        titulo: "",
+        resumen: "",
+        resto: "",
+        min: 0,
+        max: 1,
+        med: 1,
+      },
+    ],
+  },
+];
